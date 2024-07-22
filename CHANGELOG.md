ce9f51a9c1e103f24ba947f0d059c82df80b2857
DarthLilo <darthdaxx@comcast.net>
2024-07-22 03:00:52 -0700

    rewrite magnetlock system

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+# 1.1.0
+- Rewrote how the magnet lock works, now hides the interact option entirely.
+- Forgot to add BepInEx as a dependency, whoops
+
 # 1.0.0
 - Initial Release
--- Patches/MagnetLockPatch.cs
+++ Patches/MagnetLockPatch.cs
@@ -1,33 +1,73 @@
 using HarmonyLib;
+using UnityEngine;
 
 namespace MagnetLock.Patches;
 
 public class PatchMagnet
 {   
+    public static Sprite? defaultHoverIcon;
+    public static Sprite? defaultDisabledHoverIcon;
 
+    public static InteractTrigger? magnetLeverTrigger;
     public static void Initialize()
     {
         var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
-        harmony.PatchAll(typeof(MLAnimatedObjectTrigger));
+        harmony.PatchAll(typeof(StartOfRound));
     }
-    
 
-    [HarmonyPatch(typeof(AnimatedObjectTrigger))]
-    private class MLAnimatedObjectTrigger
+    [HarmonyPatch(typeof(StartOfRound))]
+    private class StartOfRoundPatch
     {
-        [HarmonyPatch("TriggerAnimation")]
-        [HarmonyPrefix]
-        private static bool TriggerAnimationPrefix(AnimatedObjectTrigger __instance)
-        {   
-            if (__instance.name == "MagnetLever")
-            { 
-                if (StartOfRound.Instance.inShipPhase)
-                {
-                    return false;
-                }
+
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        private static void UpdateMagnetStateOrbitInit()
+        {
+            var magnetLeverTriggerLocal = StartOfRound.Instance.magnetLever.GetComponent<InteractTrigger>();
+            if (magnetLeverTriggerLocal != null)
+            {
+                defaultHoverIcon = magnetLeverTriggerLocal.hoverIcon;
+                defaultDisabledHoverIcon = magnetLeverTriggerLocal.disabledHoverIcon;
+                magnetLeverTrigger = magnetLeverTriggerLocal;
             }
-            return true;
+
+            UpdateMagnetLock(false);
+        }
+
+        [HarmonyPatch("StartGame")]
+        [HarmonyPostfix]
+        private static void UpdateMagnetStateGameStart()
+        {
+            UpdateMagnetLock(true);
+        }
+
+        [HarmonyPatch("SetShipReadyToLand")]
+        [HarmonyPostfix]
+        private static void UpdateMagnetStateOrbit()
+        {
+            UpdateMagnetLock(false);
+        }
+    }
+
+    public static void UpdateMagnetLock(bool state)
+    {
+        
+        if (magnetLeverTrigger != null)
+        {
+            if (state == true)
+            {
+                magnetLeverTrigger.interactable = true;
+                magnetLeverTrigger.hoverIcon = defaultHoverIcon;
+                magnetLeverTrigger.disabledHoverIcon = defaultDisabledHoverIcon;
+
+            } else {
+                
+                magnetLeverTrigger.interactable = false;
+                magnetLeverTrigger.hoverIcon = null;
+                magnetLeverTrigger.disabledHoverIcon = null;
+            }
+            
         }
     }
     
